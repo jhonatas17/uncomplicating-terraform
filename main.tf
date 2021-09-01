2c3340054cafb7506d3792385f302839cab6b1d1
Jhonatas Santos <contato.jhonatas17@gmail.com>
2021-09-01 14:37:20 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,3 @@ provider "aws" {
   region  = "us-east-1"
   version = "~> 3.0"
 }
-
-terraform {
-  backend "s3" {
-    # Lembre de trocar o bucket para o seu, não pode ser o mesmo nome
-    bucket  = "iaasweek-terraform-jhonatas"
-    key     = "terraform-test.tfstate"
-    region  = "us-east-1"
-    encrypt = true
-  }
-}
